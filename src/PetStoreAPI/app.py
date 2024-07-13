efade4fc4bdd3b87652f9b604a2e38b48f201e77
Chalani Dissanayaka <15415@coderacademy.edu.au>
2024-07-14 02:57:36 +1000

    added new endpoints for PetStore API

--- src/PetStoreAPI/app.py
+++ src/PetStoreAPI/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from db import stores, pet_items
+from flask_smorest import abort
 import uuid
 
 app = Flask(__name__)
@@ -13,11 +14,21 @@ def get_stores():
 @app.post("/store")  # POST http://127.0.0.1:5000/store
 def create_store():
     store_data = request.get_json()
+
+    if "store_name" not in store_data:
+        abort(
+            400,
+            message="Bad request. Ensure 'store_name' is included in the JSON payload.",
+        )
+    for store in stores.values():
+        if store_data["store_name"] == store["store_name"]:
+            abort(400, message=f"Pet Store already exists.")
+
     store_id = uuid.uuid4().hex
     store = {**store_data, "id": store_id}
     stores[store_id] = store
 
-    return store, 201
+    return store
 
 
 @app.get("/store/<string:store_id>")
@@ -25,20 +36,44 @@ def get_store(store_id):
     try:
         return stores[store_id]
     except KeyError:
-        return {"message": "Pet Store not found"}, 404
+        abort(404, message="Pet Store not found.")
+
+
+@app.delete("/store/<string:store_id>")
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return {"message": "Pet Store deleted."}
+    except KeyError:
+        abort(404, message="Pet Store not found.")
 
 
 @app.post("/pet_item")  # POST http://127.0.0.1:5000/pet_item
 def create_pet_item():
     pet_item_data = request.get_json()
-    if pet_item_data["store_id"] not in stores:
-        return {"message": "Pet Store not found"}, 404
+
+    if (
+            "price" not in pet_item_data
+            or "store_id" not in pet_item_data
+            or "item_name" not in pet_item_data
+            or "item_description" not in pet_item_data
+    ):
+        abort(
+            400,
+            message="Bad request. Ensure 'price', 'store_id', 'item_name' and 'item_description' are included in the JSON payload.",
+        )
+    for pet_item in pet_items.values():
+        if (
+                pet_item_data["item_name"] == pet_item["item_name"]
+                and pet_item_data["store_id"] == pet_item["store_id"]
+        ):
+            abort(400, message=f"Pet Item already exists.")
 
     item_id = uuid.uuid4().hex
     pet_item = {**pet_item_data, "id": item_id}
     pet_items[item_id] = pet_item
 
-    return pet_item, 201
+    return pet_item
 
 
 @app.get("/pet_item")
@@ -51,4 +86,31 @@ def get_pet_item(pet_item_id):
     try:
         return pet_items[pet_item_id]
     except KeyError:
-        return {"message": "Pet Item not found"}, 404
+        abort(404, message="Pet Item not found.")
+
+
+@app.delete("/pet_item/<string:pet_item_id>")
+def delete_pet_item(pet_item_id):
+    try:
+        del pet_items[pet_item_id]
+        return {"message": "Pet Item deleted."}
+    except KeyError:
+        abort(404, message="Pet Item not found.")
+
+
+@app.put("/pet_item/<string:pet_item_id>")
+def update_pet_item(pet_item_id):
+    pet_item_data = request.get_json()
+
+    if "price" not in pet_item_data or "item_name" not in pet_item_data:
+        abort(
+            400,
+            message="Bad request. Ensure 'price', and 'item_name' are included in the JSON payload.",
+        )
+    try:
+        pet_item = pet_items[pet_item_id]
+        pet_item |= pet_item_data
+
+        return pet_item
+    except KeyError:
+        abort(404, message="Pet Item not found.")
